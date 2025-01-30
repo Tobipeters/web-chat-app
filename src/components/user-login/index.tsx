4908840b6f3b2fab66481ef1476c1fbf3a4d6f85
Oluwatobi <oluwatobilobapeterschem@gmail.com>
2025-01-30 09:31:29 +0100

    fixed up the login ui a bit

--- src/components/user-login/index.tsx
+++ src/components/user-login/index.tsx
@@ -8,20 +8,35 @@ interface ILoginProps {
 export const UserLogin: React.FC<ILoginProps> = ({ handleLogin }) => {
   const [name, setName] = React.useState<string>("");
 
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    handleLogin(name);
+  };
+
   return (
-    <div className="flex flex-col gap-4 p-4 border shadow-lg w-11/12 lg:max-w-[450px]">
-      <h3 className="text-xl text-black font-semibold mb-4 lg:text-2xl">Login to Chat</h3>
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col gap-4 p-4 border shadow-lg w-11/12 lg:max-w-[450px]"
+    >
+      <h3 className="text-xl text-black font-semibold mb-4 lg:text-2xl">
+        Login to Chat
+      </h3>
       <TextField
         fullWidth
         placeholder="Enter your username"
         name="name"
         value={name}
         onChange={({ target }) => setName(target.value)}
+        sx={{
+          "& input":{
+            padding: '0.75rem 1rem'
+          }
+        }}
       />
 
-      <Button onClick={() => handleLogin(name)} className="">
+      <Button type="submit" className="!bg-[#0f8cfb] !h-[40px] !outline-none !text-white !normal-case">
         Join chat
       </Button>
-    </div>
+    </form>
   );
 };
